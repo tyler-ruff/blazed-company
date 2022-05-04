675abe700111074f22a8149dfba73dd85452aa7d
Tyler Ruff <hello@blazed.space>
2022-05-04 13:46:07 -0400

    BLZ

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# blazed-company
-Blazed Company black hole (for the Blazed One galaxy).
+# fire-angular
+The Fire bootstrap &amp; framework library for AngularJS.
+
+## What's Included
+This package uses:
+- [AngularJS](https://angular.io/)
+- [TailwindCSS](https://tailwindcss.com/)
+
+## Installing
+To install, run the following command at the root of the directory:
+```sh
+npm install
+```
+
+## Live Development Server
+To run a live dev server, first ensure you have NPM installed. Then, if you have not already, install the Angular CLI,
+
+```sh
+npm install -g @angular/cli
+```
+
+Once installed, you can run a simple development server with the following command:
+
+```sh
+ng serve
+```
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LazyLoadDirective } from './lazyload.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LazyLoadDirective,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
