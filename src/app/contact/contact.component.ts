5b5dd99b6dd8cbf9ae0eb60e268b4e8fe0cbd6be
Tyler Ruff <hello@blazed.space>
2022-08-03 16:42:09 -0400

    BLZ

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,12 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  selectedPurpose = '';
+
+  showContact = true;
+  showSales = false;
+  showSupport = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public selectBox(values: any): void{
+    this.selectedPurpose = values.currentTarget.value;
+    if(this.selectedPurpose === 'sales' || this.selectedPurpose === 'support'){
+      this.showContact = false;
+      if(this.selectedPurpose === 'sales'){
+        this.showSales = true;
+      } else if(this.selectedPurpose === 'support'){
+        this.showSupport = true;
+      }
+    }
+  }
+  public goBack(): void{
+    this.showSales = false;
+    this.showSupport = false;
+    this.showContact = true;
+  }
   public sendMessage(values: any, form: any): void{
     form.submit();
   }
