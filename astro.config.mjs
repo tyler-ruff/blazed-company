9b6ca2be736f4059c98c7a218143f1aec892ae7a
tyler-ruff <hello@blazed.space>
2024-11-03 17:54:48 -0500

    0.0.2
    Laid the groundwork, installed Tailwind & Flowbite.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
+
+import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blazed.company',
-  output: 'server',
-  adapter: netlify(),
-  integrations: [tailwind()]
+    site: 'https://example.com',
+    integrations: [
+        mdx(), 
+        sitemap(), 
+        react(), 
+        tailwind({
+            applyBaseStyles: false,
+        })
+    ],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blazed Company';
-export const SITE_DESCRIPTION = 'Blazed Company is the corporate web network for Blazed Labs.';
-
+export const SITE_TITLE = 'Astro Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
